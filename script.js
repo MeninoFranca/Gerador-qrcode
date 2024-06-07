2ae0dd52b723e203464411341828f34f92fae5d3
MeninoFranca <baueremil9@gmail.com>
2024-06-06 21:36:15 -0300

    continuação para gerar a img do qrcode

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 document.getElementById('pixForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var valor = document.getElementById('valor').value;
+    fetch('/gerar_pix', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ valor: valor })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('qrcode').innerHTML = `<img src="data:image/png;base64,${data.qrcode}" alt="QR Code">`;
+        document.getElementById('copiaCola').innerText = `Código Copia e Cola: ${data.copiaCola}`;
+    });
 });
