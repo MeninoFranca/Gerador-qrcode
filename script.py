43931c68b1a0ecec51532fed8607b9f6d4ab8496
MeninoFranca <baueremil9@gmail.com>
2024-06-06 21:22:03 -0300

    Formatação do código PIX no padrão EMV

---
+++ script.py
@@ -0,0 +1,18 @@
+import qrcode
+import base64
+
+def gerar_pix(valor, chave_pix, nome_recebedor, cidade_recebedor, txid):
+    payload = f"""
+    000201
+    010212
+    26{len(chave_pix) + 7}0014BR.GOV.BCB.PIX{len(chave_pix):02d}{chave_pix}
+    52040000
+    5303986
+    540{len(valor)}{valor}
+    5802BR
+    5910{nome_recebedor}
+    6014{cidade_recebedor}
+    62070503***{txid}
+    6304
+    """
+ 
